8f5d53f1326f32acdae78a70949161ca86d9ccb5
JTejaSumanth <140799555+JTejaSumanth@users.noreply.github.com>
2023-09-29 21:44:14 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import AddProduct from './screens/AddProduct'
+import EditProduct from './screens/EditProduct'
+import ProductDetail from './screens/ProductDetail'
+import ShowProducts from './screens/ShowProducts'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/addProduct' component={AddProduct} />
+        <Route exact path='/products' component={ShowProducts} />
+        <Route exact path='/product/edit/:id' component={EditProduct} />
+        <Route exact path='/product/:id' component={ProductDetail} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
